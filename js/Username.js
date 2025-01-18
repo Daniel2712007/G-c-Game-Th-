08b9b1bff3db9d77b42cf774ccac37b26dde0fb3
Lycan1005 <phucdang1052@gmail.com>
2025-01-19 00:06:31 +0700

    Update Username.js

--- js/Username.js
+++ js/Username.js
@@ -1,4 +1,4 @@
 document.getElementById("name").addEventListener("input", function() {
   // Cho phép chữ cái, số và các ký tự đặc biệt
-  this.value = this.value.replace(/[^a-zA-Z0-9!@#$%^&*(),.?":{}|<>]/g, '');
+  this.value = this.value.replace(/[^a-zA-Z0-9!@#$%^&*(),.?":{}|<>_-+=]/g, '');
 });
