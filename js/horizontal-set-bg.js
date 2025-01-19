18c8fe25a8ac835ba307642475d052af824472f1
Lycan1005 <phucdang1052@gmail.com>
2025-01-19 20:22:59 +0700

    Update horizontal-set-bg.js

--- js/horizontal-set-bg.js
+++ js/horizontal-set-bg.js
@@ -6,9 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (bg) {
       // Đặt ảnh nền từ thuộc tính data-setbg
       element.style.backgroundImage = `url(${bg})`;
-      element.style.backgroundSize = "cover"; // Ảnh lấp đầy phần tử
-      element.style.backgroundPosition = "center"; // Hiển thị chính giữa
-      element.style.backgroundRepeat = "no-repeat"; // Không lặp ảnh
     }
   });
 });
