b4ca0c35385a502a2de16af8ee9b4ff209e40b78
Lycan1005 <phucdang1052@gmail.com>
2025-01-19 20:49:46 +0700

    Update horizontal-set-bg-Hoa.js

--- js/horizontal-set-bg-Hoa.js
+++ js/horizontal-set-bg-Hoa.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Chỉ áp dụng cho khung game Hoa
     const hoaImage = document.querySelector('.horizontal-hoa-image');
     if (hoaImage) {
-        const bg = hoaImage.getAttribute('data-setbg');
+        const bg = hoaImage.getAttribute('data-setbg-hoa');
         if (bg) {
             hoaImage.style.backgroundImage = `url('${bg}')`;
         }
